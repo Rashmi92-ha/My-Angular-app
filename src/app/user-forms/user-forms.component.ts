6ef313b071378972d61184df498bbf28b401c833
Rashmi92-ha <rashmiks727@gmail.com>
2026-03-13 17:36:05 +0530

    #11 form-validation for user form

--- src/app/user-forms/user-forms.component.ts
+++ src/app/user-forms/user-forms.component.ts
@@ -3,14 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-forms',
   templateUrl: './user-forms.component.html',
-  styleUrl: './user-forms.component.scss'
+  styleUrl: './user-forms.component.scss',
 })
 export class UserFormsComponent {
-  public userName = "";
-  public emailId = "";
-  onSubmit(){
-    alert("User form submitted")
-    this.userName = "";
-    this.emailId = "";
+  public userName = '';
+  public emailId = '';
+  public name = '';
+  public mail = '';
+  public phone = '';
+  public password = '';
+
+  onSubmit() {
+    alert('Submit successfull');
+    this.userName = '';
+    this.emailId = '';
+  }
+  onClick(form: any) {
+    console.log(this.name, this.mail , this.phone, this.password)
+    alert('User form submitted')
+    form.reset();
   }
 }
