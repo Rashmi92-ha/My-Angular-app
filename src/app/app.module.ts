72cefe0e4f0f1637571801f7de40c1064a514c2d
Rashmi92-ha <rashmiks727@gmail.com>
2026-03-17 20:33:15 +0530

    #13 userform creation with reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser';
 import { MainComponent } from './main/main.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
 import { UserFormsComponent } from './user-forms/user-forms.component';
+import { ReactiveFormsComponent} from './reactive-forms/reactive-forms.component';
 @NgModule({
   declarations: [
     MainComponent,
     ParentComponent,
     ChildComponent,
     UserFormsComponent,
+    ReactiveFormsComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule,ReactiveFormsModule],
   providers: [],
   bootstrap: [MainComponent],
 })
